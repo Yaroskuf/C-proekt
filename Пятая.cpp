5d8a88d53edeb6c08106cf85a1fa258b75c7254a
Yaroskuf <160023463+Yaroskuf@users.noreply.github.com>
2024-09-10 19:55:55 +0300

    Add files via upload

--- Пятая.cpp
+++ Пятая.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 using namespace std;
 
-//X-ÊÎË-ÂÎ ÑÒĞÎÊ, Y-ÊÎË-ÂÎ ÑÒÎËÁÖÎÂ
-
 int main(){
 int x;
 int y;
 int a;
-/*int y = 4;
-int x = 4;
-int m[y][x]={{-825, 100, -543, 152}, {-214, -545, 223, 141}, {208, -236, 756, -114}, {666, -488, 389, 777}};*/
+
 cout << "Insert Y: ";
 cin >> y;
 cout << "Insert X: ";
@@ -23,13 +19,7 @@ int k=0;
 int h=0;
 int no=0;
 float srz=0.0, ma=0.0;
-/*for (int i = 0; i < y; i++)
-{
-    for (int j = 0; j < x; j++) 
-    cout << m[i][j] << " ";
-    	cout << endl;
 
-}*/
 for(int i = 0; i < y; i++){
 for(int j = 0; j < x; j++){
 	  	cout << i+1 << "." << j+1 <<  ": ";
@@ -39,39 +29,25 @@ for(int j = 0; j < x; j++){
 			cout << endl;
 		}	
 }
+
+
 }
-
-
-
-
-
-//for (int i=0; i<x; i++){
-//	for (int j=0; j<y; j++){
-//		m[i][j]=i+j+2;
-//	}
-//}
-
-
-
-
 for (int i = 0; i < y; i++)
 {
     for (int j = 0; j < x; j++)
         cout << m[i][j] << " ";
      cout << endl;
 }
+cout << endl;
 
-
-
-
-for (int i=0; i<y; i++) { //âûñîòà
-	for (int j=0; j<x; j++) { //øèğèíà
+for (int i=0; i<y; i++) { 
+	for (int j=0; j<x; j++) { 
 			srz+=m[i][j];
 			k+=1;
 		if (k>y-1) {	
 			ma=max(srz/x,ma);
 			if (ma<=srz/x) {
-				no=i;
+				no=i+1;
 			}
 			k=0;
 			srz=0;
--- Седьмая.cpp
+++ Седьмая.cpp
@@ -5,9 +5,6 @@ int main() {
 int x;
 int y;
 int a;
-/*int y = 4;
-int x = 4;
-int m[y][x]={{-825, 100, -543, 152}, {-214, -545, 223, 141}, {208, -236, 756, -114}, {666, -488, 389, 777}};*/
 cout << "Insert Y: ";
 cin >> y;
 cout << "Insert X: ";
@@ -20,13 +17,8 @@ int m[y][x];
 int k=0;
 int h=0;
 int z[k];
-/*for (int i = 0; i < y; i++)
-{
-    for (int j = 0; j < x; j++) 
-    cout << m[i][j] << " ";
-    	cout << endl;
 
-}*/
+
 for(int i = 0; i < y; i++){
 for(int j = 0; j < x; j++){
 	  	cout << i+1 << "." << j+1 <<  ": ";
@@ -37,13 +29,15 @@ for(int j = 0; j < x; j++){
 		}	
 }
 }
+
+
 for (int i=0; i<y; i++){
 	for (int j=0; j<x; j++){
 		if ((m[i][j]<0 and m[i][j+1]>0) or (m[i][j]>0 and m[i][j+1]<0)) {
 			h+=1;
 		}
 		if (h==x-1){
-			cout <<  i << endl;
+			cout <<  i+1 << endl;
 		}
 		k+=1;
 		if (k==x-1){
